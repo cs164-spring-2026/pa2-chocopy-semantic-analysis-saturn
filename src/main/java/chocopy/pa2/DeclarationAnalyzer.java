a4d7e1552b5a8ac69b85efa453b527f9fa60fede
Jeremy Mills <jeremymills496@gmail.com>
2026-03-31 16:56:15 -0700

    outlined declaration analyzer

--- src/main/java/chocopy/pa2/DeclarationAnalyzer.java
+++ src/main/java/chocopy/pa2/DeclarationAnalyzer.java
@@ -2,11 +2,16 @@ package chocopy.pa2;
 
 import chocopy.common.analysis.AbstractNodeAnalyzer;
 import chocopy.common.analysis.SymbolTable;
+import chocopy.common.analysis.types.FuncType;
 import chocopy.common.analysis.types.Type;
 import chocopy.common.analysis.types.ValueType;
+import chocopy.common.astnodes.ClassDef;
 import chocopy.common.astnodes.Declaration;
 import chocopy.common.astnodes.Errors;
+import chocopy.common.astnodes.FuncDef;
+import chocopy.common.astnodes.GlobalDecl;
 import chocopy.common.astnodes.Identifier;
+import chocopy.common.astnodes.NonLocalDecl;
 import chocopy.common.astnodes.Program;
 import chocopy.common.astnodes.VarDef;
 
@@ -26,7 +31,6 @@ public class DeclarationAnalyzer extends AbstractNodeAnalyzer<Type> {
     public DeclarationAnalyzer(Errors errors0) {
         errors = errors0;
     }
-
 
     public SymbolTable<Type> getGlobals() {
         return globals;
@@ -62,5 +66,33 @@ public class DeclarationAnalyzer extends AbstractNodeAnalyzer<Type> {
         return ValueType.annotationToValueType(varDef.var.type);
     }
 
+    @Override
+    public Type analyze(FuncDef funcDef) {
+        // TODO: build FuncType from params + return type, push a new scope,
+        // recurse into funcDef.declarations for nested vars/funcs/global/nonlocal,
+        // check rule 6 (first param must match enclosing class), rule 9 (return paths)
+        return null;
+    }
+
+    @Override
+    public Type analyze(ClassDef classDef) {
+        // TODO: push a new class scope, recurse into classDef.declarations,
+        // check rule 5 (no attribute override), rule 7 (method signature match)
+        return null;
+    }
+
+    @Override
+    public Type analyze(GlobalDecl globalDecl) {
+        // TODO: record that this name refers to the global scope,
+        // check rule 3 (must actually exist in global scope)
+        return null;
+    }
+
+    @Override
+    public Type analyze(NonLocalDecl nonLocalDecl) {
+        // TODO: record that this name refers to an enclosing (non-global) scope,
+        // check rule 3 (must exist in a valid enclosing function scope)
+        return null;
+    }
 
 }
